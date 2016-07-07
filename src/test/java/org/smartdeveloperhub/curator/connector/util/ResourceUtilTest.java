6c1da7c4ff2c2e4e7dd955d69d07106fcf378905
Miguel Esteban Gutiérrez <m.esteban.gutierrez@gmail.com>
2016-07-07 10:15:54 +0200

    Bumped version number to 0.3.0-SNAPSHOT

--- src/test/java/org/smartdeveloperhub/curator/connector/util/ResourceUtilTest.java
+++ src/test/java/org/smartdeveloperhub/curator/connector/util/ResourceUtilTest.java
@@ -20,8 +20,8 @@
  *   See the License for the specific language governing permissions and
  *   limitations under the License.
  * #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
- *   Artifact    : org.smartdeveloperhub.curator:sdh-curator-connector:0.2.0-SNAPSHOT
- *   Bundle      : sdh-curator-connector-0.2.0-SNAPSHOT.jar
+ *   Artifact    : org.smartdeveloperhub.curator:sdh-curator-connector:0.3.0-SNAPSHOT
+ *   Bundle      : sdh-curator-connector-0.3.0-SNAPSHOT.jar
  * #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
  */
 package org.smartdeveloperhub.curator.connector.util;
